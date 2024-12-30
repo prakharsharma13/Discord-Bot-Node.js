e69f2239e3d3228a2526d9c4632fba376b4bcf5a
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-12-30 23:17:26 +0530

    discord bot initialization

---
+++ index.js
@@ -0,0 +1,21 @@
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+  message.reply({
+    content: "Hello from Bot",
+  });
+});
+
+client.login(process.env.DISCORD_TOKEN);
